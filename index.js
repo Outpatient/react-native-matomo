5eaa6f3832dbc51b875bd9052eb0d519bbea32b7
Lorien <lorienhw@gmail.com>
2019-06-26 14:39:02 -0700

    check native Matomo module exists before calling methods on it. We are seeing errors in production ios that sometimes the calls to the native module are failing because its undefined. Not sure why, I suspect this is an older ios OS version that is not supported by Matomo ios native. This is a patch fix to prevent these crashes until we figure out why. The call to initTracker returns false in this case so we can rollbar the error, while not crashing

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
+// var Matomo;
 var Matomo = require('react-native').NativeModules.Matomo;
 import {Platform} from 'react-native'
 module.exports = {
-  initTracker: Matomo.initTracker,
+  initTracker: function(url, id) {
+    if (!Matomo) return false;
+    Matomo.initTracker(url, id);
+    return true;
+  },
+  // initTracker: Matomo.initTracker,
   setUserId: function(userId) {
     if (userId !== null && userId !== userId !== undefined) {
-      Matomo.setUserId(userId + '');
+      Matomo && Matomo.setUserId(userId + '');
     }
   },
-  trackAppDownload: Matomo.trackAppDownload,
+  // trackAppDownload: Matomo.trackAppDownload,
+  trackAppDownload: function() {
+    Matomo && Matomo.trackAppDownload();
+  },
   trackScreen: function(path, title) {
-    Matomo.trackScreen(path, title);
+    Matomo && Matomo.trackScreen(path, title);
   },
   trackGoal: function(goalId, revenue) {
-    Matomo.trackGoal(goalId, {revenue});
+    Matomo && Matomo.trackGoal(goalId, {revenue});
   },
   trackEvent: function(category, action, name, value, url) {
-    Matomo.trackEvent(category, action, {name, value, url});
+    Matomo && Matomo.trackEvent(category, action, {name, value, url});
   },
   trackCampaign: function(name, keyword) {
-    Matomo.trackCampaign(name, keyword);
+    Matomo && Matomo.trackCampaign(name, keyword);
   },
   trackContentImpression: function(name, piece, target) {
-    Matomo.trackContentImpression(name, {piece, target});
+    Matomo && Matomo.trackContentImpression(name, {piece, target});
   },
   trackContentInteraction: function(name, interaction, piece, target) {
-    Matomo.trackContentInteraction(name, {interaction, piece, target});
+    Matomo && Matomo.trackContentInteraction(name, {interaction, piece, target});
   },
   trackSearch: function(query, category, resultCount, url) {
-    Matomo.trackSearch(query, {category, resultCount, url});
+    Matomo && Matomo.trackSearch(query, {category, resultCount, url});
   },
   trackVersion: function(version) {
     if (Platform.OS === 'ios') {
-      Matomo.set(version, 1);
+      Matomo && Matomo.set(version, 1);
     } else {
-      Matomo.setVersion(version);
+      Matomo && Matomo.setVersion(version);
     }
   },
   trackUserType: function(userType) {
     if (Platform.OS === 'ios') {
-      Matomo.set(userType, 2);
+      Matomo && Matomo.set(userType, 2);
     } else {
-      Matomo.setUserType(userType);
+      Matomo && Matomo.setUserType(userType);
     }
   }
 };
